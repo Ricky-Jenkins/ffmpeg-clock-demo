8d8c74993e50e90dad30d4c33856bfc89d081826
Ricky Jenkins <159663232+Ricky-Jenkins@users.noreply.github.com>
2025-02-14 16:19:00 +0000

    Update rtspdec.c

--- libavformat/rtspdec.c
+++ libavformat/rtspdec.c
@@ -516,9 +516,8 @@ static int rtsp_read_play(AVFormatContext *s)
 {
     AVDictionary *opts = s->metadata;
     AVDictionaryEntry *t = NULL;
-    if ((t = av_dict_get(opts, "your_option_name", NULL, 0))) {
-        // Use t->value to access the option
-        av_log(s, AV_LOG_DEBUG, "Option from metadata: %s\n", t->value);
+    if ((t = av_dict_get(opts, "range_clock", NULL, 0))) {
+        av_log(s, AV_LOG_DEBUG, "[*****]Option from metadata: %s\n", t->value);
     }
 
     RTSPState *rt = s->priv_data;
